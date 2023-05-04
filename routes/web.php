44f31daf4745fddf4598e93f405f2122fd2f1d09
dbedoya <dannygonzalobedoya@gmail.com>
2023-05-04 15:56:06 -0500

    Comienzo lecciones de uno a 19 laravel

---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return "Hola desde la pagina de inicio de danny";
+    //return view('welcome');
+});
+
+
+Route::get('/contacto', function () {
+    return "hola desde la pagina contactos usando el metodo get";
+
+});
+
+
+Route::post('/contacto', function () {
+    return "hola desde la pagina contactos usando el metodo post";
+
+});
+
+
+Route::match(['get', 'post'],'/contacto', function () {
+    return "hola desde la pagina contactos usando el metodo get o post";
+
+});
+
+
+Route::get('/cursos/informacion', function() {
+    return "Aqui podras encontrar toda la informacion de los cursos";
+
+});
+
+
+Route::get('/cursos/{curso}', function($curso) {
+    return "Bienvenido al curso: $curso";
+});
+
+
+Route::get('/cursos/{curso}/{category}', function($curso, $category){
+    return "Bienvenido al curso: $curso de la categoria: $category";
+
+});
+
+//para el caso que uno de los parametros sean opcionales
+
+Route::get('/cursos/{curso}/{category?}', function($curso, $category = null){
+
+    if($category){
+        return "Bienvenido al curso: $curso de la categoria: $category";
+    }else{
+        return "Bienvenido al curso: $curso";
+    }
+    
+
+});
+
+//para proteger los datos que se ingresan con expresiones regulares
+
+Route::get('/cursos/{curso}/{category}', function($curso, $category){
+    return "Bienvenido al curso: $curso de la categoria: $category";
+})->where ('cursos', '[a-zA-Z]+');
